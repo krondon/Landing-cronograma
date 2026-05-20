5b9f73dc848f69a25c8534951b071d48c0bd646a
unknown <danielecd18@gmail.com>
2026-05-20 10:27:53 -0400

    chore: remove capacitor and android artifacts

--- android/app/src/main/java/com/morna/cronograma/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.morna.cronograma;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.morna.cronograma',
-  appName: 'Expo FedeIndustria',
-  webDir: 'dist'
-};
-
-export default config;
